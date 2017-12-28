a2ddc7853954b817caed4b90ff32aedbb4f3ace3
c3443955 Asfand Khan <A.khan8766@student.leedsbeckett.ac.uk>
2017-12-28 13:47:08 +0000

    login page

---
+++ DB_System/Form3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace DB_System
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\c3443955\Documents\myDB.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter dataadp = new SqlDataAdapter("select count (*) from login where username = '" + textBox1.Text + "' and password='" + textBox2.Text + "'", connection);
+            DataTable dta = new DataTable();
+            dataadp.Fill(dta);
+            if (dta.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("please enter correct username and password", "alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
